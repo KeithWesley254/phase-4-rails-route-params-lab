4809ef680560ce8e06183f9ba446ef2a6a3e9be5
Keith <keithwesley187@gmail.com>
2022-09-26 20:36:05 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def specific_student
+    specific = Student.all.find(params[:id])
+    render json: specific
+  end
+
 end
